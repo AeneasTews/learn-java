690a279ce47b3b282442bb080765d9a29efdfd5c
AeneasTews <Aeneas.tews@gmail.com>
2022-01-30 22:14:13 +0100

    User input
    The Scanner class included in the java utilities can be used to get user input at runtime. By instantiating an object of type Scanner it is possible to use a variety of methods to get different types of input. The scanner class has multiple methods for getting different inputs ranging from single bytes to whole Lines. Check out https://www.w3schools.com/java/java_user_input.asp for more details.

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MyClass
 {
     public static void main(String[] args)
@@ -8,5 +10,9 @@ public class MyClass
         int integer = 4;
         String string = "This is a string";
         double floating_point_number = 4.23;
+
+        //user input
+        Scanner myVar = new Scanner(System.in);
+        System.out.println(myVar.nextLine());
     }
 }
