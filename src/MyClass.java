2fb5ccb941cd3ab7f08d2bdd617fb6dfef8be770
AeneasTews <Aeneas.tews@gmail.com>
2022-01-30 20:51:03 +0100

    Variables
    Java has different variable types. These are very similar to other languages. I have added an integer, a String (notice the capitalization), and a floating point number. While the keyword float does exist, the default type for floating point numbers in Java is double. Like in most other languages, a double simply stores more data than float (64-bit vs. 32-bit). Check out this https://www.educba.com/java-float-vs-double/ article for more information.

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,5 +3,10 @@ public class MyClass
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
+        //variables
+        int integer = 4;
+        String string = "This is a string";
+        double floating_point_number = 4.23;
     }
 }
